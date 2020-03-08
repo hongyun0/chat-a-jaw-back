38aaafae5d01c5acecbda2fac08f694bf97b7cbd
kmkim0817 <kmkim0817@linecorp.com>
2020-03-08 21:28:47 +0900

    Add gradle dependencies for jsoup library

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	implementation("org.jsoup:jsoup:1.13.1")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
